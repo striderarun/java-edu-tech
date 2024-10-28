9cc320dcd1c723c6fa3e65ac43177b39fd2e41fa
striderarun <strider90arun@gmail.com>
2024-10-28 12:01:42 -0700

    Autograder constants

---
+++ src/main/java/com/codelearner/common/Constants.java
@@ -0,0 +1,8 @@
+package com.codelearner.common;
+
+public class Constants {
+
+    public static final String AUTO_GRADER = "AutoGrader";
+    public static final String AUTO_GRADER_FILE = "AutoGrader.java";
+
+}
