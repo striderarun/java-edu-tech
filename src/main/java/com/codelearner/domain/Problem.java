36047e3460efe1b06ba26c694799ce67c558e835
striderarun <strider90arun@gmail.com>
2024-10-30 15:51:30 -0700

    The problem model

---
+++ src/main/java/com/codelearner/domain/Problem.java
@@ -0,0 +1,20 @@
+package com.codelearner.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Problem {
+
+	private String id;	  
+	private MetaData metadata;
+	private String filename;
+	private String contentType;
+	private Long chunkSize;
+	private Long length;
+	private Date uploadDate;
+	private String md5;
+	private String aliases;
+
+}
