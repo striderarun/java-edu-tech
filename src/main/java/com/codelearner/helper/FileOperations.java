ef238584d2840d25274654e4e58440dd1ea6aebd
striderarun <strider90arun@gmail.com>
2024-10-31 09:35:27 -0700

    Interface method for Compile and check for errors

--- src/main/java/com/codelearner/helper/FileOperations.java
+++ src/main/java/com/codelearner/helper/FileOperations.java
@@ -1,2 +1,25 @@
-package com.codelearner.helper;public interface FileOperations {
+package com.codelearner.helper;
+
+import java.io.File;
+import java.util.List;
+
+public interface FileOperations {
+
+    /**
+     * Create a Java source file from problem code
+     *
+     * @param codeLines
+     * @param className
+     * @param language
+     * @return
+     */
+    File createFile(List<String> codeLines, String className, String language);
+
+    /**
+     * Compile and check for errors
+     *
+     * @param file
+     * @return
+     */
+    List<String> syntaxChecker(String file);
 }
